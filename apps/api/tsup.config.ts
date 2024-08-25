36ec2c8ed8523f1d11726b321c4e81c3f7fc7075
VallenDraa <jestinevallendradwiputra@gmail.com>
2024-08-25 14:59:07 +0700

    build(api): edit tsup config to bundle all `@repo` modules

--- apps/api/tsup.config.ts
+++ apps/api/tsup.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, type Options } from "tsup";
 
 export default defineConfig((options: Options) => ({
   entryPoints: ["src/index.ts"],
+  noExternal: ["@repo"],
   clean: true,
   format: ["cjs"],
   ...options,
