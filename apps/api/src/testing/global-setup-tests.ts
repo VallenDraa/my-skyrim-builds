d7c80258cb9f859e675f3915a202441f5adde44b
VallenDraa <jestinevallendradwiputra@gmail.com>
2024-08-27 11:58:59 +0700

    test(api): setup test environment with vitest

---
+++ apps/api/src/testing/global-setup-tests.ts
@@ -0,0 +1,4 @@
+import type { GlobalSetupContext } from "vitest/node" with { "resolution-mode": "import" };
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function -- Not yet implemented
+export default function setup(_: GlobalSetupContext): void {}
